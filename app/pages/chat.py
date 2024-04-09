d6a7a1fee5cf1cccf9566a26164f3cb5f3e04431
Gabriel Dischon <gabrielgdischon@gmail.com>
2024-04-09 09:41:16 +0300

    updated gitignore2

---
+++ app/pages/chat.py
@@ -0,0 +1,71 @@
+from taipy.gui import Markdown, State
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+# edit context
+
+context = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today? "
+conversation = {
+    "Conversation": ["Who are you?", "Hi! I am GPT-3. How can I help you today?"]
+}
+current_user_message = ""
+
+client = openai.Client(api_key = os.getenv("OPENAI_API_KEY"))
+
+def request(state: State, prompt: str) -> str:
+    response = state.client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": f"{prompt}",
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    return response.choices[0].message.content
+
+def send_message(state: State) -> None:
+    """
+    Send the user's message to the API and update the conversation.
+
+    Args:
+        - state: The current state.
+    """
+    # Add the user's message to the context
+    state.context += f"Human: \n {state.current_user_message}\n\n AI:"
+    # Send the user's message to the API and get the response
+    answer = request(state, state.context).replace("\n", "")
+    # Add the response to the context for future messages
+    state.context += answer
+    # Update the conversation
+    conv = state.conversation._dict.copy()
+    conv["Conversation"] += [state.current_user_message, answer]
+    state.conversation = conv
+    # Clear the input field
+    state.current_user_message = ""
+
+chat_md = Markdown("""
+<|{conversation}|table|show_all|style=style_conv|>
+<|{current_user_message}|input|label=Write your message here...|on_action=send_message|class_name=fullwidth|>
+""")
+
+def style_conv(state: State, idx: int, row: int) -> str:
+    """
+    Apply a style to the conversation table depending on the message's author.
+
+    Args:
+        - state: The current state of the app.
+        - idx: The index of the message in the table.
+        - row: The row of the message in the table.
+
+    Returns:
+        The style to apply to the message.
+    """
+    if idx is None:
+        return None
+    elif idx % 2 == 0:
+        return "user_message"
+    else:
+        return "gpt_message"
